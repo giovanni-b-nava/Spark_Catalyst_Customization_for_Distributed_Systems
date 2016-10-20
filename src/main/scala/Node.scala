459309abcff7867cd923c6bc4d4e062c12f0044d
V4c0n <ant.cosseddu@gmail.com>
2016-10-20 16:44:35 +0200

    Initial commit

---
+++ src/main/scala/Node.scala
@@ -0,0 +1,13 @@
+/**
+  * Created by Spark on 19/10/2016.
+  */
+class Node(val name: String,
+           val types: String,
+           val performance: Map[String, Double],
+           val costs: Map[String, Double],
+           val links: Map[String, Double],
+           val plaintext: Array[String],
+           val encrypted: Array[String])
+{
+
+}
