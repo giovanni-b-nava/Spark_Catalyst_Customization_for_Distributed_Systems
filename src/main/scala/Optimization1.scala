e53e8884d7e850ce27a5265576b518f9742f8277
Giovanni <giovanni.b.nava@gmail.com>
2016-10-27 15:38:57 +0200

    Update json parsing

--- src/main/scala/Optimization1.scala
+++ src/main/scala/Optimization1.scala
@@ -1,7 +1,5 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.catalyst.expressions.{Literal, Multiply}
-import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
-import org.apache.spark.sql.catalyst.rules.Rule
+import scala.util.parsing.json._
+import scala.io.Source
 /**
   * Created by Spark on 27/10/2016.
   */
@@ -9,13 +7,24 @@ object Optimization1 {
 
   def main(args: Array[String]): Unit =
   {
-
+    /*
     val sparkSession = SparkSession.builder.
       master("optimization")
       .appName("rule1")
       .getOrCreate()
 
     val dataFrame = sparkSession.read.json("src/main/resources/DB_Employees.json")
+    */
+
+    val jsonSource : String = Source.fromFile("src/main/resources/DB_config.json").getLines.mkString
+
+    val result = JSON.parseFull(jsonSource)
+    result match {
+      // Matches if jsonStr is valid JSON and represents a Map of Strings to Any
+      case Some(map: Map[String, Any]) => println(map)
+      case None => println("Parsing failed")
+      case other => println("Unknown data structure: " + other)
+    }
 
   }
 }
